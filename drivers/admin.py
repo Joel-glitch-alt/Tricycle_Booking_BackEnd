c150dade9b4ef361cec08a531ad745d2cfd056c3
Joel-glitch-alt <joeladdition@gmail.com>
2026-04-15 16:50:16 +0000

    updated

--- drivers/admin.py
+++ drivers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Driver
 
-# Register your models here.
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    # These are the columns you will see in the list
+    list_display = ('full_name', 'vehicle', 'rating', 'is_available')
+    # This adds the search bar
+    search_fields = ('full_name', 'vehicle')
+    # This adds the filter sidebar
+    list_filter = ('is_available',)
--- drivers/serializers.py
+++ drivers/serializers.py
@@ -1,15 +1,7 @@
-# serializers.py
 from rest_framework import serializers
-
-from drivers.models import Driver
-
+from .models import Driver
 
 class DriverSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
-
     class Meta:
         model = Driver
         fields = ['id', 'name', 'vehicle', 'rating', 'trips', 'is_available']
-
-    def get_name(self, obj):
-        return obj.user.get_full_name()
