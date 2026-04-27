4c9b0034bb701de75c49db008f422fc3fe3dca66
Joel-glitch-alt <joeladdition@gmail.com>
2026-04-27 13:00:59 +0000

    updated

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'payments'
+
 urlpatterns = [
     path('initialize/', views.InitializePaymentView.as_view(), name='initialize-payment'),
     path('verify/<str:reference>/', views.VerifyPaymentView.as_view(), name='verify-payment'),
     path('webhook/', views.paystack_webhook, name='paystack-webhook'),
 ]
+
