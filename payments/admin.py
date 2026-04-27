3ea628631f9a4a0d234503a617728a30183e2822
Joel-glitch-alt <joeladdition@gmail.com>
2026-04-27 12:48:57 +0000

    payment on admin dash board

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Payment
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('reference', 'user', 'booking', 'amount', 'method', 'status', 'created_at')
+    list_filter = ('status', 'method')
+    search_fields = ('reference', 'user__username')
