a25c371b3fd2ff4b102a46c916ab5755e8ceecea
Joel-glitch-alt <joeladdition@gmail.com>
2026-04-30 14:08:22 +0000

    Fix static files settings

--- tricycle_backend/settings.py
+++ tricycle_backend/settings.py
@@ -102,7 +102,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ add this
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ── Custom User Model ──────────────────────────────────────────
